98cbba121217d4478e981c34d469e752e1e573b9
NickMatvienko <inmonkeywetrust@gmail.com>
2017-01-29 01:26:30 +0200

    Vertex_homework #1. Creating shapes using design patterns. (Factory, Builder, Singleton, Proxy, Decorator)

--- src/collections/SomeClass.java
+++ src/collections/SomeClass.java
@@ -1,9 +1,15 @@
 package collections;
 
+import java.util.Random;
+
 /**
  * Created by Микола on 22.01.2017.
  */
 public class SomeClass {
+    public static void main(String[] args) {
+        Random r = new Random ();
+        System.out.println(r.nextInt(4));
+    }
 
 
 }
